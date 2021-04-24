30feac0bf8ff598a5ea8a47e767dd022b4ac9264
ymcastellar <yoycas@hotmail.com>
2021-04-24 18:13:24 -0500

    adding main file and complements

---
+++ go.mod
@@ -0,0 +1,5 @@
+module polyline-test
+
+go 1.16
+
+require github.com/twpayne/go-polyline v1.0.1
---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+
+	polyline "github.com/twpayne/go-polyline"
+)
+
+func main() {
+	/*	coords := [][]float64{
+		{38.5, -120.2},
+		{40.7, -120.95},
+		{43.252, -126.453},
+	}*/
+
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	var location [][]float64
+
+	for scanner.Scan() {
+		re2 := regexp.MustCompile(`Location\sarrived\s\<(.+)\>`)
+		match := re2.FindStringSubmatch(scanner.Text())
+		if len(match) > 1 {
+
+			splitedmatch := strings.Split(match[1], ",")
+
+			if lat, err0 := strconv.ParseFloat(splitedmatch[0], 64); err0 == nil {
+				if long, err1 := strconv.ParseFloat(splitedmatch[1], 64); err1 == nil {
+					location = append(location, []float64{lat, long})
+				}
+			}
+		}
+
+	}
+
+	fmt.Println(string(polyline.EncodeCoords(location)))
+}
